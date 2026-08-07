7db183ca6729760ec148473a9258b92d68c339f0
cpainagent007 <cpainagent007@gmail.com>
2026-08-06 19:55:44 -0600

    Initial Scripts
    Added Note, Measure, Section, Chart, Highway, Singleton, and managers

---
+++ Assets/Scripts/Charting/Section.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A section in the song. Contains a list of measures.
+/// The name of the section must be unique.
+/// </summary>
+[CreateAssetMenu(fileName = "NewSection", menuName = "BW/SectionDefinition")]
+public class Section : ScriptableObject
+{
+    public string sectionName;
+    public List<Measure> measures;
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
